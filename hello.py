36eef33c4e81e8a3c673039b5d205764149c35cc
kenneally15 <kenneally15@yahoo.com>
2025-02-26 17:59:29 -0500

    add and mult functions

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+def add(x,y):
+	return x+y
+
+def mult(x,y):
+	return x*y
+
 def main():
 	print("Hello world!")
 	print("Test script")
@@ -7,7 +13,8 @@ def main():
 	x = 2
 	y = 3
 
-	print(f"{x} + {y} =",x+y)
+	print(f"{x} + {y} =",add(x,y))
+	print(f"{x}*{y}=",mult(x,y))
 
 
 if __name__ == '__main__':
