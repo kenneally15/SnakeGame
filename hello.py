1b1cca88eca55c8932b4fd18dada85e484c8c62b
kenneally15 <kenneally15@yahoo.com>
2025-02-27 15:25:26 -0500

    human class import update

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from human import Human
-
+import human as h
 
 def add(x,y):
 	return x+y
@@ -19,9 +18,9 @@ def main():
 	print(f"{x} + {y} =",add(x,y))
 	print(f"{x}*{y}=",mult(x,y))
 
-	l = Human()
-	k = Human("Kevin")
-	m = Human("Marianne",2)
+	l = h.Human()
+	k = h.Human("Kevin")
+	m = h.Human("Marianne",2)
 
 	k.say("hello")
 
